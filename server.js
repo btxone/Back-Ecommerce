8b7ea862b5b0039a0b50fe8d4c899532d436a68b
btxone <btxpablo@gmail.com>
2023-01-14 17:36:46 -0300

    commit1

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const server = require('./app')
+
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`)
+});
