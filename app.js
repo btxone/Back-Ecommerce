a0ee0a9f859a89e19f6404c19f0c0c8d83ca84bd
btxone <btxpablo@gmail.com>
2023-01-14 17:58:23 -0300

    primer commit

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 
+const productRouter = require('./routes/product')
+// const categoryRouter = require('./routes/category')
+// const userRouter = require('./routes/user')
+// const reviewsRouter = require('./routes/reviews')
+// const cartRouter = require('./routes/cart')
+ const mercadoPagoRouter = require('./routes/mercadoPago')
+const morgan = require('morgan')
 const server = express();
 
+
+server.use(morgan("dev"));
+
+
+
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.use('/', mercadoPagoRouter)
+
+server.use('/', productRouter)
 
 
 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
 const server = require('./app')
+const dbConnect = require('./db')
+// server.use(morgan("dev"));
+
 
 
 const PORT = process.env.PORT || 3001;
@@ -8,3 +11,5 @@ const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en puerto ${PORT}`)
 });
+
+dbConnect();
